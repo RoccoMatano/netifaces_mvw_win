efa0c23042a6b980e4d5dd15956cf809039260fe
Rocco Matano <rocco@matano.de>
2025-02-19 07:27:56 +0100

    muck out

--- _build_me.cmd
+++
@@ -1,5 +0,0 @@
-setlocal
-call setvc
-set DISTUTILS_USE_SDK=yes
-set py_vcruntime_redist=yes
-py setup.py bdist_wheel
--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 import sys
+import subprocess
 from pathlib import Path
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 
 ################################################################################
 
 if sys.platform != "win32":
     raise OSError("no support for non-Windows OS")
 
-MIN_PY_VER = "3.6"
+MIN_PY_VER = "3.8"
 
 MIN_MAJOR, MIN_MINOR = map(int, MIN_PY_VER.split("."))
 LIMITED_API = f"0x{MIN_MAJOR:02x}{MIN_MINOR:02x}0000"
@@ -19,19 +21,22 @@ __version__ = "0.11.0"
 
 iface_mod = Extension(
     "netifaces",
-    sources=["entry.c", "netifaces.c"],
+    sources=["src/entry.c", "src/netifaces.c"],
     libraries=["ws2_32", "iphlpapi", "kernel32", "ucrt", "vcruntime"],
-    extra_compile_args=["/O1", "/Os", "/GS-",],
+    extra_compile_args=["/O1", "/Os", "/GS-"],
     extra_link_args = [
         "/manifest:no",
         "/emittoolversioninfo:no",
         "/nocoffgrpinfo",
         "/entry:entry_point",
-        "/stub:roma.stub",
+        "/stub:src/roma.stub",
+        "/opt:ref",
+        "/opt:icf",
         "/section:.vanish,R",
         "/merge:.xdata=.vanish",
         "/merge:.pdata=.vanish",
         "/ignore:4253",
+        "/ignore:4075",
         ],
     define_macros=[
         ("NETIFACES_VERSION", __version__),
@@ -41,38 +46,25 @@ iface_mod = Extension(
     py_limited_api=True
     )
 
-with open(Path(__file__).parent / "README.rst", "r") as fp:
-    long_desc = fp.read()
+################################################################################
+
+class hacked_build_ext(build_ext):
+    # hack to post-process ext after linking
+    def build_extension(self, ext):
+        ext_path = self.get_ext_fullpath(ext.name)
+        super().build_extension(ext)
+        subprocess.run(["squab.exe", ext_path])
+        Path(ext_path + ".bak").unlink(missing_ok=True)
+
+cmd_class = {'build_ext': hacked_build_ext}
 
 ################################################################################
 
 setup (
-    name="netifaces",
     version=__version__,
-    zip_safe=True,
-    description="Portable network interface information.",
-    license="MIT License",
-    long_description=long_desc,
-    author="Alastair Houghton",
-    author_email="alastair@alastairs-place.net",
-    url="https://github.com/al45tair/netifaces",
-    python_requires=f">={MIN_PY_VER}",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Topic :: System :: Networking",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        ],
     ext_modules=[iface_mod],
     options={'bdist_wheel': {'py_limited_api': LIMITED_API_TAG}},
+    cmdclass=cmd_class,
     )
 
 ################################################################################
